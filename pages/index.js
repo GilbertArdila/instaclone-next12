bec39ae74381a61cc0df6f40357a9e254b3a66c7
Gilbert Ardila <gilbertferney@gmail.com>
2023-12-25 12:25:57 -0500

    add some images and changing the next version from 13  to 12

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div >
+    <div>
       <Head>
         <title>InstaClone App</title>
         <meta name="InstaClone" content="only for educational purposes" />
         <link rel="icon" href="/logo.svg" />
       </Head>
 
-      <main >
+      <main>
         <h1 className="font-mono text-red-500">Hello word</h1>
       </main>
     </div>
-  )
+  );
 }
