32cedfeb002c36188ea09b56d7f36e5f7733c1b2
Gilbert Ardila <gilbertferney@gmail.com>
2023-12-29 07:37:15 -0500

    stories styles, use tailwind-scrollbar to hide the scrollbar

--- components/feed/Stories.jsx
+++ components/feed/Stories.jsx
@@ -21,7 +21,7 @@ const Stories = () => {
     
     
   return (
-    <div>
+    <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border overflow-x-scroll scrollbar-none rounded-sm">
         {storyUsers.map((story)=>(
            <Story key={story.id} user={story}/>
         ))}
--- components/feed/Story.jsx
+++ components/feed/Story.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+
 
 const Story = ({user}) => {
   return (
-    <>
-     <img src={user.image} alt={user.userName} />
-     <p>{user.userName}</p>
-    </>
+    <div>
+     <img 
+     className="rounded-full h-14 w-14 p-[1.5px] border-2 border-x-red-500 border-y-blue-500 cursor-pointer hover:scale-110 transition-transform duration-200 ease-in "
+     src={user.image} 
+     alt={user.userName} />
+     <p className="truncate text-xs w-14">{user.userName}</p>
+    </div>
   )
 }
 
