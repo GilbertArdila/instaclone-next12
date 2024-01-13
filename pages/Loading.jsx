0a82b7028a386df741449e7f2a1ce67cc9eae4b9
Gilbert Ardila <gilbertferney@gmail.com>
2024-01-13 07:59:56 -0500

    uploading post to firestore, Loading component}

---
+++ pages/Loading.jsx
@@ -0,0 +1,12 @@
+const Loading = () => {
+  return (
+    <div>
+        <img 
+        className="w-16 h-16 rounded-full border-collapse border-2 border-red-300 "
+        src="/Spinner.svg" 
+        alt="loading Spinner" />
+    </div>
+  )
+}
+
+export default Loading;
